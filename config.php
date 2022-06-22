39b11147e87ad4f462f8f84828ae58c21b33b8f1
Ramen-Nood1ez <rommelc000@gmail.com>
2022-06-22 09:45:54 -0500

    #56

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 	define('DB_SERVER', 'localhost');
-	define('DB_USERNAME', 'root');
-	define('DB_PASSWORD', '');
+	define('DB_USERNAME', 'hogoshaj_carter');
+	define('DB_PASSWORD', 'F53MiNGPB6QrXbGgEB3T');
 	define('DB_NAME', 'hogoshaj_fileshare');
 
 	$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
