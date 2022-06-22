9932057d25a08e9c8e33777e27ffd51a5f796c89
Ramen-Nood1ez <rommelc000@gmail.com>
2022-06-21 21:51:04 -0500

    #19

--- index.php
+++ index.php
@@ -1,34 +1,24 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Document</title>
-	<link rel="stylesheet" href="style.css">
-</head>
-<body>
-	<?php
-		ini_set("include_path", '/home/hogoshaj/php:' . ini_get("include_path") );
-		echo "test!<br>";
-		$directory = scandir("./tournament/Tohkon_2022/");
-		foreach	($directory as $item) {
-			echo "$item <br>";
-		}
+<?php
+	$dom = new DOMDocument();
+	$dom->loadHTMLFile("/default.html");
 
-		$selected = "/tournament/Tohkon_2022/Aimana_Daniyar_vs_Peyton_Lang.MOV";
+	function changesource($newsource, $dom) {
+		$dom = new DOMDocument();
+		$dom->loadHTMLFile("/default.html");
 
 		echo "<video width='320' height='240' controls>\n";
-		echo "\t <source src=$selected type='video/mp4'>\n";
-		echo "</video> <br>"
-	?>
+		echo "\t <source src=$newsource type='video/mp4'>\n";
+		echo "</video> <br>";
+	}
 
-	<br>
+	$directory = scandir("./tournament/Tohkon_2022/");
+	foreach	($directory as $item) {
+		echo "$item <br>";
+	}
 
-	<video controls>
-		<source 
-		src="/tournament/Tohkon_2022/Aimana_Daniyar_vs_Peyton_Lang.MOV" 
-		type="video/mp4">
-	</video>
-</body>
-</html>
+	$selected = "/tournament/Tohkon_2022/Aimana_Daniyar_vs_Peyton_Lang.MOV";
+
+	echo "<video width='320' height='240' controls>\n";
+	echo "\t <source src=$selected type='video/mp4'>\n";
+	echo "</video> <br>";
+?>
