0e5cdb98439587487cff8630430d5b5daa56adb3
Ramen-Nood1ez <rommelc000@gmail.com>
2022-06-21 22:43:59 -0500

    #42

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
 		global $dom;
 		$dom = new DOMDocument();
 		$dom->loadHTML('<!DOCTYPE html>\n<html lang="en">\n<head>\n\t<meta charset="UTF-8">\n\t<meta http-equiv="X-UA-Compatible" content="IE=edge">\n\t<meta name="viewport" content="width=device-width, initial-scale=1.0">\n\t<title>Document</title>\n\t<link rel="stylesheet" href="style.css">\n</head>\n<body>\n');
+		echo $dom;
 	}
 
 	function finish() {
