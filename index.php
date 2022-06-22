85d8e5b0cbe9e61a2d814d2be8a245f0b2569d68
Ramen-Nood1ez <rommelc000@gmail.com>
2022-06-21 22:46:31 -0500

    #44

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 	function init() {
 		global $dom;
 		$dom = new DOMDocument();
-		echo '<!DOCTYPE html>\n<html lang="en">\n<head>\n\t<meta charset="UTF-8">\n\t<meta http-equiv="X-UA-Compatible" content="IE=edge">\n\t<meta name="viewport" content="width=device-width, initial-scale=1.0">\n\t<title>Document</title>\n\t<link rel="stylesheet" href="style.css">\n</head>\n<body>\n';
+		echo '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>Hogosha Judo File Share</title><link rel="stylesheet" href="style.css"></head><body>';
 	}
 
 	function finish() {
